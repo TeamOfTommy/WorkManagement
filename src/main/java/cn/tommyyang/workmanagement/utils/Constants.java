84b09e2ce26e60b00d9a2f44cf2f8152ae54357a
joyang1 <tingzai.yang@gmail.com>
2019-09-20 12:52:27 +0800

    optimize code

--- src/main/java/cn/tommyyang/workmanagement/utils/Constants.java
+++ src/main/java/cn/tommyyang/workmanagement/utils/Constants.java
@@ -1,5 +1,12 @@
 package cn.tommyyang.workmanagement.utils;
 
+import cn.tommyyang.slf4j4json.Logger;
+import cn.tommyyang.slf4j4json.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * @Author : TommyYang
  * @Time : 2019-09-11 18:11
@@ -8,8 +15,23 @@ package cn.tommyyang.workmanagement.utils;
  */
 public class Constants {
 
-    public static final String WEB_ROOT = "http://localhost:8080/WorkManagement_war_exploded";
+    private static final String CONFIG_FILE = "constants.properties";
+    private static Properties properties = new Properties();
 
-    public static final String FILE_DIR = "/Users/tommy/Documents/tmp";
+    private static final Logger LOG = LoggerFactory.getLogger(Constants.class);
+
+    static {
+        InputStream inStream = Constants.class.getClassLoader().getResourceAsStream(CONFIG_FILE);
+        try {
+            properties.load(inStream);
+        } catch (IOException e) {
+            LOG.error().strField("event", "load-constants").strField("error", e.getMessage())
+                    .exception("\nexception:\n", e).log();
+        }
+    }
+
+    public static final String WEB_ROOT = properties.getProperty("web_root", "http://fengrui.snailsheep.com");
+
+    public static final String FILE_DIR = properties.getProperty("excel_dir", "/data/wwwroot/fengrui.snailsheep.com/excels");
 
 }
